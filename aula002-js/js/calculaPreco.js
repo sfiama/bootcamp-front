7f9df99a0180a3fc80670ae8d5f65ba34a5cd9d7
Sophia Fiama <fiama.sophia.sf@gmail.com>
2022-07-21 13:41:35 -0300

    ex revisao js

---
+++ aula002-js/js/calculaPreco.js
@@ -0,0 +1,31 @@
+function calculaFrete (preco){
+    let frete; 
+    if(preco > 1 && preco<=2000 ){
+    frete = 300;
+    } else if(preco > 2000 && preco<=4000 ){
+        frete = 450;
+    }else if(preco > 4000){
+        frete = 700;
+    }else {
+        return " Não foi possivel calcular o frete, pedimos que entre em contato com nossa central de relacionamento."
+    }
+    return frete
+
+}
+
+function calculaTotal(preco){
+    let frete = calculaFrete(preco);
+    let valorFinal = preco + frete;
+    return valorFinal
+}
+
+function calcularPreco(produto, preco){
+    let total = calculaTotal(preco);
+    return `O produto R$ ${produto} custa R$ ${preco}. Seu custo de envio é
+    R$ 
+    ${total-preco}. Portanto, o preço final é R$ ${total}'`;
+}
+
+console.log(calcularPreco('Macbook', 2500));
+console.log(calcularPreco('Celular', 500));
+console.log(calcularPreco('Playstation', 4500));
