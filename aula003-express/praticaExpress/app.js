a5dcdba7e72cdab541a71f9be882ad1fc663c00f
sfiama <sophia.fiama@mercadolivre.com>
2022-07-25 18:12:01 -0300

    Exercicio atividade aula 02

---
+++ aula003-express/praticaExpress/app.js
@@ -0,0 +1,84 @@
+// A importação de express, do array de produtos e instanciação do express no app
+const express = require('express');
+let produtos = require('./produtos');
+const app = express();
+
+// Vai pegar e responder com todos os produtos
+app.get('/produtos', (req, res) => {
+  res.status(200).json(produtos);
+});
+
+// Ele pega a informação de um único produto, e os paraâmetro de details é opcional
+app.get('/produtos/:id/:details?', (req, res) => {
+    const id = Number(req.params.id);
+  
+    const details = req.params.details || 'Sem descrição';
+    console.log(details);
+  
+    const product = produtos.find((produto) => produto.id === id)
+  
+    res.status(200).json(product);
+  })
+
+// independente do método (POST/PUT), vai cair nessa rota que faz a rtadução do body
+app.use(express.json());
+
+// Adiciona um produto, esse funciona, pq passou pelo express.json()
+app.post('/produtos', (req, res) => {
+  const content = req.body;
+
+  produtos = [...produtos, content];
+
+  res.status(201).json(produtos);
+})
+
+// Uma rota POST que não funciona, porque não identifica o body, precisa passar pelo express.json()
+app.post('/produtos1', (req, res) => {
+    const content = req.body;
+  
+    const newProducts = [...produtos, content];
+  
+    res.status(201).json(newProducts);
+  })
+
+
+// PUT faz a atualização de um objeto de acordo com o ID
+app.put('/produtos/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const content = req.body;
+
+  const product = produtos.find((produto) => produto.id === id);
+
+  if (!product) {
+    return res.status(400).json({ "message": "Produto não encontrado" })
+  }
+
+  const produtoAtualizado = produtos.map((produto) => {
+    if (produto.id === id) return content;
+
+    return produto;
+  })
+
+  produtos = produtoAtualizado;
+
+  res.status(200).json(produtos);
+})
+
+// DELETE deleta o produto de id que seja o igual ao da parametro
+app.delete('/produtos/:id', (req, res) => {
+  const id = Number(req.params.id);
+
+  const product = produtos.find((produto) => produto.id === id);
+
+  if (!product) {
+    return res.status(400).json({ "message": "Produto não encontrado" })
+  }
+
+  produtos = produtos.filter((produto) => produto.id !== id );
+
+  return res.status(200).json(produtos);
+})
+
+app.listen(3001, () => {
+  console.log('Servidor online');
+})
---
+++ aula003-express/praticaExpress/produtos.js
@@ -0,0 +1,10 @@
+let products = [
+    { name: 'Macbook', price: 1300, id: 1, description: 'Macbook'},
+    { name: 'Iphone', price: 1000, id: 2, description: 'Iphone'},
+    { name: 'Pendrive', price: 10, id: 3, description: 'Pendrive'},
+    { name: 'Headset', price: 50, id: 4, description: 'Headset'},
+    { name: 'Mouse', price: 20, id: 5, description: 'Mouse'},
+
+    ]
+
+    module.exports = products;
